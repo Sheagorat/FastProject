b221fb4ffae7aec45e7a8fd330da946d4d92bbfb
Sheagorat <blubblob120@gmail.com>
2024-12-04 03:11:05 +0300

    Delete Project Generation directory

--- Project Generation/script.js
+++
@@ -1,7 +0,0 @@
-const words = ["исторяи", "биология", "география", "физика", "химия", "обществознание", "литература"];
-
-document.getElementById('generate-word').addEventListener('click', function() {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    const randomWord = words[randomIndex];
-    document.getElementById('word-output').innerText = randomWord;
-});
